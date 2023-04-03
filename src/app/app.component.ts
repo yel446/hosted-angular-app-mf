92a38c02e2ba18ee067e1f32ccd3a3c9ac027787
YAMB LANDRY <landryyamb446@gmail.com>
2023-04-03 15:11:52 +0000

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'hosted-angular-app-wp';
+  remote_url: string = 'http://localhost:4200/micro-fe.js';
+  remote_react_url: string =
+    'https://remote-app-wp-test.vercel.app/remoteEntry.js';
 }
